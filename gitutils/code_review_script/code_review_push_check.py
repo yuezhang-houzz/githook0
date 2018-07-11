00121c74141878c3bf043b8995c21de3d1fda4f2
yue.zhang <yue.zhang@houzz.com>
2018-07-11 10:52:49 -0700

    add release version check in pre push

--- gitutils/code_review_script/code_review_push_check.py
+++ gitutils/code_review_script/code_review_push_check.py
@@ -9,12 +9,17 @@ misc_check = Misc_Check()
 github = Github()
 phabricator = Phabricator()
 bypass = Bypass()
+release_cut_off_check = Release_cut_off_check()
 BYPASS = sys.argv[1] if len(sys.argv) > 1 else 0
 
 
 if __name__ == '__main__':
 	# No need for check review if repo is clean
 	misc_check.repo_clean_check()
+
+	# Check if release version is correct
+	if constants.HAS_CUTOFF:
+		release_cut_off_check.release_cutoff_check("push")
 
 	# Force code review
 	misc_check.branch_check()
